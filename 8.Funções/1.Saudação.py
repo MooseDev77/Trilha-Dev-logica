ee93bbe78fcc084435694142d9233381524c17bb
MooseDev77 <luigisantos.oficial@gmail.com>
2025-05-21 13:49:12 -0300

    ok

--- 8.Funções/1.Saudação.py
+++
@@ -1,8 +0,0 @@
-import os 
-os.system("clear")
-
-def saudacao(paramento):
-    print(f"Olá, {paramento}! Bem-vindo(a) oa nosso site.")
-
-nome_visitante = input("digite o seu nome: ")
-saudacao(nome_visitante)
