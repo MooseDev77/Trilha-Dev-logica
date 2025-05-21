89b4680001475c9b0d2ecd874ddeaeeeb47e4aa3
MooseDev77 <luigisantos.oficial@gmail.com>
2025-05-21 19:53:57 +0000

    revisar os novos assuntos

---
+++ javascript/1.variaveis.js
@@ -0,0 +1,13 @@
+// Variáveis
+// Codigo atual
+let nome = 'Marta'
+
+nome = "Maria"
+
+// exibir no console
+console.log(nome)
+
+
+// Codigo legado (antigo)
+var nome = 'marta'
+console.log(`NOme: ${nome}`)
---
+++ javascript/2.constante.js
@@ -0,0 +1,12 @@
+// Constante
+Const idade = 20
+
+// Tentando mudar a idade
+// idade = 1
+
+// Exibindo resultado
+console.log('idade: ', idade)
+console.log(`idade:  ${idade}`)
+
+
+
